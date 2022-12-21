3196b1dc9f8e22a9ed08def6f5ad11b7f3486c00
samuel0412 <samuel@yeasitech.com>
2022-12-21 15:06:08 +0530

    dropdown

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+  const dropDownToggle = () => {
+    const dropDownListSec = document.querySelector(".dropDownListSec");
+    dropDownListSec.classList.toggle("open");
+  };
+  return (
+    <>
+      <div className="mainSec">
+        <div className="dropDownListSec" onClick={dropDownToggle}>
+          <span className="dropDownHeaderText">Dropdown list</span>
+          <span className="dropDownHeaderIcon">
+            <i className="fa-solid fa-chevron-down"></i>
+          </span>
+        </div>
+        <div className="dropDownList">
+          <ul>
+            <li>Number</li>
+            <li>String</li>
+            <li>BigInt</li>
+            <li>Null</li>
+            <li>Boolean</li>
+            <li>Array</li>
+            <li>Object</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
